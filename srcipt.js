ec527a1f6a83ec9fb8785e328a11fe5c49c1c3f5
Pixellly <fs.karaa@gmail.com>
2025-07-18 16:05:07 +0400

    Added styling, improved logic, and fixed bugs mentioned in 'gotchas' section

--- srcipt.js
+++ srcipt.js
@@ -1,13 +1,4 @@
-// let num1, num2, operator
-// sumButton.addEventListener('click', () => {
-//     const exp = prompt('Enter 2 numbers to add', '2+2')
-//     const parts = exp.split('+')
-
-//     const a = parseInt(parts[0].trim())
-//     const b = parseInt(parts[1].trim())
-
-//     alert(sum(a,b))
-// })
+let leftOperand=''; let operator=null; let rightOperand = ''; let newOperation = false;
 
 function sum(a,b){
     return a+b
@@ -25,11 +16,19 @@ function divide(a,b){
     return a/b
 }
 
-function operate(operator, num1, num2){
-    if (operator==="+"){ return sum(num1, num2)}
-    if (operator==="-"){ return subtract(num1, num2)}
-    if (operator==="*"){ return multiply(num1, num2)}
-    if (operator==="/"){ return divide(num1, num2)}
+function operate(operator, a, b){
+    const num1 = parseFloat(a)
+    const num2 = parseFloat(b)
+    if (operator==="+"){ return Math.round(sum(num1, num2)* 1000) / 1000}
+    if (operator==="-"){ return Math.round(subtract(num1, num2)* 1000) / 1000}
+    if (operator==="*"){ return Math.round(multiply(num1, num2)* 1000) / 1000}
+    if (operator==="/"){ 
+        if(b==0){
+            alert('Division by 0 = bad');
+            clear();
+            return null;
+        }else {return Math.round(divide(num1, num2)* 1000) / 1000}
+    }
 }
 
 const screen = document.querySelector('.screen')
@@ -53,42 +52,85 @@ const nine = document.querySelector('.nine')
 const zero = document.querySelector('.zero')
 const decimal = document.querySelector('.decimal')
 
-clearButton.addEventListener('click', () => screen.innerText ="")
+clearButton.addEventListener('click', clear)
+
+sumButton.addEventListener('click', () => display("+"))
+subButton.addEventListener('click', () => display("-"))
+multButton.addEventListener('click', () => display("*"))
+divButton.addEventListener('click', () => display("/"))
+
+one.addEventListener('click', ()=> display(1))
+two.addEventListener('click', ()=> display(2))
+three.addEventListener('click', ()=> display(3))
+four.addEventListener('click', ()=> display(4))
+five.addEventListener('click', ()=> display(5))
+six.addEventListener('click', ()=> display(6))
+seven.addEventListener('click', ()=> display(7))
+eight.addEventListener('click', ()=> display(8))
+nine.addEventListener('click', ()=> display(9))
+zero.addEventListener('click', ()=> display(0))
+decimal.addEventListener('click', ()=> display('.'))
+
+function clear(){
+    screen.innerText ="";
+    leftOperand = ''; operator=null; rightOperand='';
+}
+
+
+function display(exp) {
+    if (exp === '.') {
+        if (!operator) {
+            if (!leftOperand.includes('.')) {
+                leftOperand += '.';
+                screen.innerText = leftOperand;
+            }
+        } else {
+            if (!rightOperand.includes('.')) {
+                rightOperand += '.';
+                screen.innerText = rightOperand;
+            }
+        }
+        return;
+    }
+
+    if (!isNaN(exp)) {
+        if (!operator) {
+            if(newOperation){
+                clear();
+                leftOperand += exp;
+                screen.innerText = leftOperand;
+                newOperation = false
+            } else{
+            leftOperand += exp;
+            screen.innerText = leftOperand;
+        }
+        } else {
+            rightOperand += exp;
+            screen.innerText = rightOperand;
+        }
+        return;
+    }
+
+    if (!operator && leftOperand) {
+        operator = exp;
+    } else if (operator && rightOperand) {
+        // chain operation: 0.1 + 0.9 + 1
+        let result = operate(operator, leftOperand, rightOperand);
+        leftOperand = result.toString();
+        operator = exp;
+        rightOperand = '';
+        screen.innerText = leftOperand
+    }
+}
+
 
 equalButton.addEventListener('click', () => {
-    const parts = screen.innerText.match(/(\d*\.\d+|\d+|[+\-*/()])/g)
-
-    const num1 = parseFloat(parts[0].trim())
-    const operator = parts[1].trim()
-    const num2 = parseFloat(parts[2].trim())
-
-    if (operator === "+"){
-        screen.innerText = sum(num1, num2)
-    }
-     if (operator === "-"){
-        screen.innerText = subtract(num1, num2)
-    }
-     if (operator === "*"){
-        screen.innerText = multiply(num1, num2)
-    }
-     if (operator === "/"){
-        screen.innerText = divide(num1, num2)
+    if(rightOperand){
+        screen.innerText = operate(operator, leftOperand, rightOperand)
+        leftOperand = screen.innerText 
+        rightOperand = ''
+        operator = null
+        newOperation= true
     }
 })
 
-sumButton.addEventListener('click', () => screen.innerText+="+")
-subButton.addEventListener('click', () => screen.innerText+="-")
-multButton.addEventListener('click', () => screen.innerText+="*")
-divButton.addEventListener('click', () => screen.innerText+="/")
-
-one.addEventListener('click', ()=> screen.innerText+=1)
-two.addEventListener('click', ()=> screen.innerText+=2)
-three.addEventListener('click', ()=> screen.innerText+=3)
-four.addEventListener('click', ()=> screen.innerText+=4)
-five.addEventListener('click', ()=> screen.innerText+=5)
-six.addEventListener('click', ()=> screen.innerText+=6)
-seven.addEventListener('click', ()=> screen.innerText+=7)
-eight.addEventListener('click', ()=> screen.innerText+=8)
-nine.addEventListener('click', ()=> screen.innerText+=9)
-zero.addEventListener('click', ()=> screen.innerText+=0)
-decimal.addEventListener('click', ()=> screen.innerText+='.')
